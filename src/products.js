fc66f48bf218beef293e4b616f31543792b43ac8
nadiaaida <94579202+nadiaaida@users.noreply.github.com>
2023-09-06 09:10:47 +0800

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/1.jpg';
+import product2 from './assets/2.jpg';
+import product3 from './assets/3.jpg';
+import product4 from './assets/4.jpg';
+import product5 from './assets/5.jpg';
+import product6 from './assets/6.jpg';
+import product7 from './assets/7.jpg';
+import product8 from './assets/8.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 5000.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Camera",
+        price: 3000.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Shirt",
+        price: 60.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Cap",
+        price: 19.9,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Laptop",
+        price: 4400.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Watch",
+        price: 1599.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Book",
+        price: 39.99,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Shoe",
+        price: 157.0,
+        productImage: product8,
+    },
+];
